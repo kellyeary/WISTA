0d7d5a67b7a0b8d9fa7615e027d1970bde4e622e
Kelly <kellyeary@gmail.com>
2018-12-31 12:24:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+
+$( "#1Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".1Drop").removeClass("hide");
+      $(".1Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#2Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".2Drop").removeClass("hide");
+      $(".2Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#3Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".3Drop").removeClass("hide");
+      $(".3Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#4Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".4Drop").removeClass("hide");
+      $(".4Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#5Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".5Drop").removeClass("hide");
+      $(".5Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#6Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".6Drop").removeClass("hide");
+      $(".6Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#7Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".7Drop").removeClass("hide");
+      $(".7Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+$( "#8Plus" ).click(function (e) {
+      $(".active").removeClass("active");
+      $(".show").removeClass("show").addClass("hide");
+      $(".8Drop").removeClass("hide");
+      $(".8Drop").addClass("show");
+      $(this).addClass("active");
+e.preventDefault();
+});
+
+});
